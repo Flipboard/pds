9f47a459302e777c0b182a86a9383541ed915a98
Ubuntu <ubuntu@ip-172-30-151-226.ec2.internal>
2025-10-07 20:00:07 +0000

    put back in

--- updater_post_install.sh
+++ updater_post_install.sh
@@ -95,9 +95,7 @@ sudo chown -R ubuntu:ubuntu /home/bluesky
 
 # get packages
 cd ${SRC_DIR}/service
-#source /home/ubuntu/.bashrc
-#source /home/ubuntu/.nvm/nvm.sh
-#npm install --production --frozen-lockfile
+npm install --production --frozen-lockfile
 ln -s /home/bluesky/pds.env .
 
 exit 0
